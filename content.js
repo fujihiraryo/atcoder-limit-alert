e65a46f01eb2ffee2a5510973d9af3514f91d754
hotarunx <29631883+hotarunx@users.noreply.github.com>
2020-10-10 13:49:29 +0900

    fix for website specification

--- content.js
+++ content.js
@@ -2,14 +2,14 @@ window.onload = function () {
     var allTags = document.getElementsByTagName('p');
     for (var tag of allTags) {
         if (tag.textContent.match(/実行時間制限/)) {
-            if (!tag.textContent.startsWith('実行時間制限: 2 sec / メモリ制限: 1024 MB')) {
+            if (!tag.textContent.match('実行時間制限: 2 sec / メモリ制限: 1024 MB')) {
                 tag.style.color = "red"
                 tag.style.fontSize = "xx-large"
                 break;
             }
         }
         if (tag.textContent.match(/Time Limit*/)) {
-            if (!tag.textContent.startsWith('Time Limit: 2 sec / Memory Limit: 1024 MB')) {
+            if (!tag.textContent.match('Time Limit: 2 sec / Memory Limit: 1024 MB')) {
                 tag.style.color = "red"
                 tag.style.fontSize = "xx-large"
                 break;
